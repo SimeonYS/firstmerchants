c49ee26887dcb93c7d8e7aea89e248c719518556
simeon.simeonov <simeon.simeonov@adata.pro>
2021-04-05 11:19:36 +0300

    Done

---
+++ README.md
@@ -0,0 +1,9 @@
+URL: https://www.firstmerchants.com/news/1
+
+Spider name: firstmerchants
+DB Schema:
+
+date
+title
+link
+content
---
+++ firstmerchants/spiders/spider.py
@@ -0,0 +1,37 @@
+import re
+import scrapy
+from scrapy.loader import ItemLoader
+from ..items import FfirstmerchantsItem
+from itemloaders.processors import TakeFirst
+
+pattern = r'(\xa0)?'
+
+class FfirstmerchantsSpider(scrapy.Spider):
+	name = 'firstmerchants'
+	start_urls = ['https://www.firstmerchants.com/news/1']
+
+	def parse(self, response):
+		post_links = response.xpath('//article/a/@href').getall()
+		yield from response.follow_all(post_links, self.parse_post)
+
+		next_page = response.xpath('//a[@title="Next Page"]/@href').get()
+		if next_page:
+			yield response.follow(next_page, self.parse)
+
+	def parse_post(self, response):
+		date = response.xpath('//p[@class="date"]/text()').get()
+		date = re.findall(r'\w+\s\d+\,\s\d+', date)
+		title = response.xpath('//h1/text()').get()
+		content = response.xpath('//div[@class="news-description"]//text()').getall()
+		content = [p.strip() for p in content if p.strip()]
+		content = re.sub(pattern, "",' '.join(content))
+
+		item = ItemLoader(item=FfirstmerchantsItem(), response=response)
+		item.default_output_processor = TakeFirst()
+
+		item.add_value('title', title)
+		item.add_value('link', response.url)
+		item.add_value('content', content)
+		item.add_value('date', date)
+
+		yield item.load_item()
